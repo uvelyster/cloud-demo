99c66bde26e3774d3afdce447414e2a0d984c20f
uvelyster <48861250+uvelyster@users.noreply.github.com>
2023-06-08 00:29:10 +0900

    Update create_db_webtest.sql

--- create_db_webtest.sql
+++ create_db_webtest.sql
@@ -3,4 +3,4 @@ grant all privileges on *.* to 'root'@'10.0.1.%' identified by 'Test123!';
 flush privileges;
 create database webtest default charset=utf8 collate=utf8_bin;
 use webtest;
-create table topic ( id int(11) not null auto_increment, title varchar(45) not null, description text, created datetime not null, primary key(id) );
+create table items ( id int(11) not null auto_increment, title varchar(45) not null, description text, created datetime not null, primary key(id) );
