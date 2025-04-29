5992210dc8e071dc5329b768b8550330fffddf79
uvelyster <48861250+uvelyster@users.noreply.github.com>
2025-04-29 19:53:59 +0900

    Update process_create.php

--- process_create.php
+++ process_create.php
@@ -1,10 +1,14 @@
 <?php
 
-$conn = mysqli_connect("DB IP","root","Test123!","webtest",3306);
+$conn = mysqli_connect("DB IP","testuser","testpass","webtest",3306);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 $sql = "insert into items (title, description, created) value ('{$_POST['title']}','{$_POST['description']}', now())";
 
-mysqli_query($conn,$sql);
+$result = mysqli_query($conn,$sql);
 if ($result=== false){
     echo 'error occured.';
     error_log(mysqli_error($conn));
